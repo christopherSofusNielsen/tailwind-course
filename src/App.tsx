eecb7c01dd33172092623207021378022cf45298
christopherSofusNielsen <c.consult.christopher@gmail.com>
2022-03-06 15:03:23 +0100

    navbar done

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
-import { Intro } from "./components/Intro";
+// import { Intro } from "./components/Intro";
+import { Header } from "./components/navbar/Header";
 
 const App = () => {
-  return <Intro />;
+  // return <Intro />;
+  return <Header />;
 };
 
 export default App;
---
+++ src/components/navbar/Header.tsx
@@ -0,0 +1,45 @@
+import { HiMenu, HiUserCircle } from "react-icons/hi";
+
+export const Header = () => {
+  return (
+    <nav className="flex items-center justify-between w-screen p-2 h-14 bg-slate-50">
+      <div className="basis-1/3">
+        <span className="p-1 cursor-pointer">
+          <HiMenu className="w-8 h-8 ml-2 fill-cyan-700" />
+        </span>
+      </div>
+      <div className="text-center basis-1/3">
+        <a href="/" className="text-2xl font-semibold text-cyan-700">
+          MERLIN
+        </a>
+      </div>
+      <div className="flex items-center justify-end h-full basis-1/3">
+        <Link title="Projects" />
+        <Link title="Workspace" />
+        <LoginLink />
+      </div>
+    </nav>
+  );
+};
+
+const LoginLink = () => {
+  return (
+    <a href="/" className="">
+      <span className="flex items-center h-full p-1 font-semibold rounded text-cyan-700 hover:bg-slate-300 ">
+        Login
+        <HiUserCircle className="w-8 h-8 ml-2 fill-cyan-700" />
+      </span>
+    </a>
+  );
+};
+
+const Link = ({ title }: { title: string }) => {
+  return (
+    <a
+      href="/"
+      className="items-center hidden h-full p-1 mx-2 font-semibold rounded text-cyan-700 hover:bg-slate-300 md:flex"
+    >
+      {title}
+    </a>
+  );
+};
